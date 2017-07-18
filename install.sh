05219744922af1bcd4929db64a34c73a66ad6cee
kris27mc <kris27mc@gmail.com>
2017-07-17 21:33:50 -0400

    Added install script, icon, and desktop launcher

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#Determines CPU
+if grep -qi "amd" /proc/cpuinfo;  then
+  git checkout master-AMD
+fi
+
+#Compiles
+cmake .
+make
+
+#Checks for complete build
+if ! ls | grep -q "mcpelauncher"; then
+  echo "Error: mcpelauncher missing. Build failed"
+  exit
+fi
+
+#Moves compiled files to new dir
+mkdir ~/mcpelauncher
+cp -t ~/mcpelauncher mcpelauncher extract.sh LICENSE mcpe.desktop
+cp -r libs ~/mcpelauncher/libs
+cd ~/mcpelauncher
+
+#Acquires apk
+git clone https://github.com/MCMrARM/Google-Play-API.git
+cd Google-Play-API
+cmake .
+make
+./gplaydl -tos -a com.mojang.minecraftpe
+cp *.apk ~/mcpelauncher
+cd ~/mcpelauncher
+rm -R Google-Play-API
+
+#Extracts apk into assets
+if [-f "mcpe.apk"]; then
+  mkdir oldapks
+  mv mcpe.apk oldapks
+fi
+mv *.apk mcpe.apk
+./extract.sh mcpe.apk
+
+#Creates desktop launcher
+ cp mcpe.desktop ~/Desktop
+ 
