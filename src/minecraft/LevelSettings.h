c8bd45604d968fb987b5b56677905fa891404902
MCMrARM <mcmrarm@gmail.com>
2018-01-13 14:18:47 +0100

    Fixed commands not being enabled in the server

--- src/minecraft/LevelSettings.h
+++ src/minecraft/LevelSettings.h
@@ -14,9 +14,14 @@ public:
     bool hasAchievementsDisabled; // 18
     int dimension; // 1c
     int time; // 20
-    bool edu; // 21
+    bool edu, immutableWorld; // 21, 22
     float rainLevel, lightningLevel; // 28, 2c
-    bool mpGame, lanBroadcast, xblBroadcast, commandsEnabled, texturepacksRequired, overrideSavedSettings; // 2d, 2e, 2f, 30, 31, 32~34
+    bool mpGame, lanBroadcast, xblBroadcast; // 2d, 2e, 2f
+    int xblLanBroadcastMode; // 34
+    bool platformBroadcast, commandsEnabled; // 35, 36
+    bool texturepacksRequired, lockedBehaviourPack, lockedResourcePack, fromLockedTemplate; // 37, 38, 39, 3A
+    bool overrideSavedSettings; // 3B
+    bool bonusChestEnabled, startWithMap; // 3C, 3D
     char filler[0x300];
 
     LevelSettings();
