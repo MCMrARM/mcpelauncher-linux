fe05497b10c90fd79059b2174573284a55edd9a5
MCMrARM <mcmrarm@gmail.com>
2018-03-30 19:10:30 +0200

    Fixed file picking

--- src/minecraft/FilePickerSettings.h
+++ src/minecraft/FilePickerSettings.h
@@ -10,12 +10,12 @@ struct FilePickerSettings {
         NONE, OPEN, SAVE
     };
     struct FileDescription {
-        std::string ext, desc;
+        mcpe::string ext, desc;
     };
 
     char filler [0x10]; // 10
     std::function<void (FilePickerSettings&)> cancelCallback; // 20
-    std::function<void (FilePickerSettings&, std::string)> pickedCallback; // 30
+    std::function<void (FilePickerSettings&, mcpe::string)> pickedCallback; // 30
     std::vector<FileDescription> fileDescriptions; // 3c
     int filler3; // 40
     PickerType type; // 44
